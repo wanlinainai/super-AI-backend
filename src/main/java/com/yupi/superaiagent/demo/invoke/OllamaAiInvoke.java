64f3c16204bb38f5d273ab00bcd7633a128b2cc2
zxh <zxh15166423670@gmail.com>
2025-06-08 23:10:02 +0800

    feat invoke test

---
+++ src/main/java/com/yupi/superaiagent/demo/invoke/OllamaAiInvoke.java
@@ -0,0 +1,28 @@
+package com.yupi.superaiagent.demo.invoke;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName: OllamaAiInvoke
+ * @Author: zxh
+ * @Date: 2025/6/8 23:03
+ * @Description: ollama 调用
+ */
+@Component
+public class OllamaAiInvoke implements CommandLineRunner {
+
+    @Resource
+    private ChatModel ollamaChatModel;
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage output = ollamaChatModel.call(new Prompt("你好，我是李白，请帮我生成一段诗歌，必须是七言绝句"))
+                .getResult()
+                .getOutput();
+        System.out.println(output.getText());
+    }
+}
---
+++ src/main/java/com/yupi/superaiagent/demo/invoke/SpringAiAiInvoke.java
@@ -0,0 +1,27 @@
+package com.yupi.superaiagent.demo.invoke;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName: SpringAiAiInvoke
+ * @Author: zxh
+ * @Date: 2025/6/8 22:38
+ * @Description: 阿里的模型调用invoke （取消注释即可在SpringBoot项目启动时执行）
+ */
+@Component
+public class SpringAiAiInvoke implements CommandLineRunner {
+    @Resource
+    private ChatModel dashscopeChatModel;
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage output = dashscopeChatModel.call(new Prompt("你好，我是林彪，请帮我写一周关于6月4号的七言绝句"))
+                .getResult()
+                .getOutput();
+        System.out.println(output.getText());
+    }
+}
